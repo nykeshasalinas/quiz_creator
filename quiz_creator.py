975a99a4ae51f8e3598daf962aa245ca31871824
nykeshasalinas <salinasnykesha12@gmail.com>
2025-04-09 16:49:16 +0800

    initialized a variable to open the text file where quiz data will be imported

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,8 +1,9 @@
 # Program a quiz creator
+text_file = open("quiz_created.txt", "+w")
 
 # Create a loop that asks user to input question
 while True:
-    question = input(f"Input Quiz Question: ")
+    question = input("Input Quiz Question: ")
     # Ask user to input options a, b, c, and d
     print("Input Options: ")
     option_a = input("a. ")
@@ -21,6 +22,6 @@ while True:
         # Go back to loop if Yes
         continue
     else:
-        exit
+        exit()
         # Else, exit the program
     # Save the quiz in the text file
