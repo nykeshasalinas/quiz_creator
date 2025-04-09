2aa5edb5f8956dcb214360a0ddaffc18e0f1c992
nykeshasalinas <salinasnykesha12@gmail.com>
2025-04-09 16:32:18 +0800

    added pseudocode

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,1 +1,10 @@
 # Program a quiz creator
+
+# Create a loop that asks user to input question
+# Ask user to input options a, b, c, and d
+# Ask user to input the correct answer among the options
+# Import user inputs to text file
+# Ask the user if they want to add question
+    # Go back to loop if Yes
+    # Else, exit the program
+# Save the quiz in the text file
