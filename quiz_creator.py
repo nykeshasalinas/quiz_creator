622b36b140310ab09221c6f2e22fcdf267d1cd75
nykeshasalinas <salinasnykesha12@gmail.com>
2025-04-09 16:36:16 +0800

    added user inputs for question and answer options

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,10 +1,18 @@
 # Program a quiz creator
 
 # Create a loop that asks user to input question
-# Ask user to input options a, b, c, and d
-# Ask user to input the correct answer among the options
-# Import user inputs to text file
-# Ask the user if they want to add question
-    # Go back to loop if Yes
-    # Else, exit the program
-# Save the quiz in the text file
+while True:
+    question = input(f"Input Quiz Question: ")
+    # Ask user to input options a, b, c, and d
+    print("Input Options: ")
+    option_a = input("a. ")
+    option_b = input("b. ")
+    option_c = input("c. ")
+    option_d = input("d. ")
+    
+    # Ask user to input the correct answer among the options
+    # Import user inputs to text file
+    # Ask the user if they want to add question
+        # Go back to loop if Yes
+        # Else, exit the program
+    # Save the quiz in the text file
