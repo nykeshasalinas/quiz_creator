ed643f49f377766b575a47b7e67144ca16bbefa8
nykeshasalinas <salinasnykesha12@gmail.com>
2025-05-07 22:34:57 +0800

    revised pseudocode

--- quiz_app.py
+++ quiz_app.py
@@ -1,4 +1,5 @@
-# Import Tkinter library for application's GUI
+# Import tkinter library for application's GUI
+# Import random module to randomize questions
 # Read questions in quiz_created.txt
 # Generate the questions randomly and allow the user to answer
 # Check if the user's answer is correct
