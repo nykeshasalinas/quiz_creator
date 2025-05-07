9d0302edc8a6489f0a0b16b38c8195f26fd0226e
nykeshasalinas <salinasnykesha12@gmail.com>
2025-05-07 22:17:22 +0800

    added pseudocode

---
+++ quiz_app.py
@@ -0,0 +1,5 @@
+# Import Tkinter library for application's GUI
+# Read questions in quiz_created.txt
+# Generate the questions randomly and allow the user to answer
+# Check if the user's answer is correct
+# Generate the user's score
