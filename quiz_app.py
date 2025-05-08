866a4680fa6658e0439ac2e8099c90ddd99f82f9
nykeshasalinas <salinasnykesha12@gmail.com>
2025-05-09 01:13:28 +0800

    added pseudocode

--- quiz_app.py
+++ quiz_app.py
@@ -40,6 +40,8 @@ if __name__ == "__main__":
         print(f"Answer: {q.answer}")
         print()
 
+# Make the the app's GUI using tkinter library
+
 # Generate the questions randomly and allow the user to answer
 # Check if the user's answer is correct
 # Generate the user's score
